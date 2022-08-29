5f30aa0666c4e7677f1b275883545b0817266008
bractom <bractomweb@gmail.com>
2022-08-30 01:20:50 +0900

    最低限のメニューと機能を実装
    ボタン押下でCSSのセット・リセットが可能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
+import { ref, watch } from 'vue';
+
+const replaceCss = '/replace.css';
+
+const isEnabled = ref(false);
+
+watch(isEnabled, async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const param = {
+    target: { tabId: tab.id || 0 },
+    files: [ replaceCss ],
+  };
+
+  await chrome.scripting[isEnabled.value ? 'insertCSS' : 'removeCSS'](param);
+});
 </script>
 
 <template>
-hello world.
+  <h1>Zenn Dark Theme</h1>
+  <button @click="isEnabled = !isEnabled">{{ isEnabled ? 'Disable' : 'Enable'}}</button>
 </template>
 
 <style scoped>
+label {
+  font-size: 20px;
+}
 </style>
